fad468a47a8e3d5a014ca8e445617125f8608275
mazouari <mohamed.ali.zouari@gmail.com>
2023-11-06 23:08:17 +0100

    update project

--- src/test/java/com/e2eTest/automation/step_definitions/AuthentificationStepDefinition.java
+++ src/test/java/com/e2eTest/automation/step_definitions/AuthentificationStepDefinition.java
@@ -1,29 +1,51 @@
 package com.e2eTest.automation.step_definitions;
 
+import com.e2eTest.automation.page_objects.AuthentificationPage;
+import com.e2eTest.automation.utils.ConfigFileReader;
+import com.e2eTest.automation.utils.SeleniumUtils;
+
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class AuthentificationStepDefinition {
+	
+	private AuthentificationPage authentificationPage;
+	private SeleniumUtils seleniumUtils;
+	private ConfigFileReader configFileReader;
+	
+	public AuthentificationStepDefinition() {
+		seleniumUtils = new SeleniumUtils();
+		authentificationPage = new AuthentificationPage();
+		configFileReader = new ConfigFileReader();
+	}
 
+	/*TC_01*/
 	@Given("Je me connecte sur l application nopCommerce")
 	public void jeMeConnecteSurLApplicationNopCommerce() {
+		seleniumUtils.get(configFileReader.getProperties("home.recette"));
 	}
 
 	@When("Je saisis l email {string}")
-	public void jeSaisisLEmail(String string) {
+	public void jeSaisisLEmail(String email) {
+		seleniumUtils.writeText(AuthentificationPage.getEmail(), email);
 	}
 
-	@When("Je saisis le password {string}")
-	public void jeSaisisLePassword(String string) {
+	@And("Je saisis le password {string}")
+	public void jeSaisisLePassword(String password) {
+		seleniumUtils.writeText(AuthentificationPage.getPassword(), password);
 	}
 
 	@When("Je clique sur le bouton Log In")
 	public void jeCliqueSurLeBoutonLogIn() {
+		seleniumUtils.click(AuthentificationPage.getBtnLogin());
 	}
 
 	@Then("Je me redirige vers la page Home")
 	public void jeMeRedirigeVersLaPageHome() {
 	}
+	
+	/*TC_02*/
 
 }
---
+++ src/test/java/com/e2eTest/automation/step_definitions/CatalogStepDefinition.java
@@ -0,0 +1,5 @@
+package com.e2eTest.automation.step_definitions;
+
+public class CatalogStepDefinition {
+
+}
